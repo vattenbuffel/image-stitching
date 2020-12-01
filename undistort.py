82e68b1da85db5c26b295af663f651cf8230afa7
vattenbuffel <nois97.noa@gmail.com>
2020-12-01 17:46:08 +0100

    fully remove black lines

--- undistort.py
+++ undistort.py
@@ -150,47 +150,98 @@ for lambda_ in good_eig_vals:
 smallest_err_index = np.argmin(np.abs(error_values))
 lambda_ = good_eig_vals[smallest_err_index]
 
-# Undistort the image
-n_rows = img1.shape[0]
-n_cols = img1.shape[1]
-
-# Calculate locations of p in img1 and img2
-p_img1 = np.zeros((2, np.prod(img1.shape)))
-
-# create the different x_coordinates and y_coordinates values and normalize them
-x_coordinates = (np.arange(n_cols) - x_max/2)/(n_rows+n_cols)
-x_coordinates = npm.repmat(x_coordinates, 1, n_rows)
-y_coordinates = (np.arange(n_rows) - y_max/2)/(n_rows+n_cols)
-y_coordinates = npm.repmat(y_coordinates.reshape(-1,1), 1, n_cols).reshape(-1)
-
-p_img1[0] = x_coordinates
-p_img1[1] = y_coordinates
-
-# Undistort the coordinates
-z_values = (1 + lambda_*(p_img1[0]**2+p_img1[1]**2))
-p_img1 = np.true_divide(p_img1[0:2, :], z_values)
-
-# Scale back the coordinates
-p_img1[0] *= (n_rows+n_cols)
-p_img1[1] *= (n_rows+n_cols)
-
-p_img1 = np.round(p_img1)
-p_img1 = np.ndarray.astype(p_img1, dtype='int')
-
-# Create a cv img where the undistorted image gets the correct colors, from the distorted image
-n_cols_distorted = img1.shape[1]
-
-x_max = p_img1[0].max()
-x_min = p_img1[0].min()
-y_max = p_img1[1].max()
-y_min = p_img1[1].min()
-
-n_rows_undistorted = int(y_max-y_min+1)
-n_cols_undistorted = int(x_max-x_min+1)
-new_img = (p_img1.transpose() - np.array([x_min, y_min], dtype='int')).transpose()
+# Color correct
+# Find the max and min coordiantes. They are either on the middle of the edges or in the corners
+x_max_original = img1.shape[1]
+y_max_original = img1.shape[0]
+n_cols_original = x_max_original
+n_rows_original = y_max_original
 
 
-undistorted_image = np.zeros((n_rows_undistorted, n_cols_undistorted, 3))
-undistorted_image[new_img[1], new_img[0]] = img1_rgb.reshape(-1,3)
+# not sure if these are correct
+if lambda_ > 0 : # pincushion distortion
+    extreme_points = np.array([[x_max_original/2, x_max_original, x_max_original/2, 0], [0, y_max_original/2, y_max_original, y_max_original/2]], dtype = 'float')
+else: # barrel distortion
+    extreme_points = np.array([[0, x_max_original, x_max_original, 0], [0, 0, y_max_original, y_max_original]], dtype = 'float') 
+    
+# Normalize the points 
+extreme_points[0] -=x_max_original/2
+extreme_points[1] -= y_max_original/2
+extreme_points /= (x_max_original + y_max_original)
+
+# Calculate z and undistort the points
+z = (1 + lambda_*(extreme_points[0]**2+extreme_points[1]**2))
+extreme_points /= z
+
+# Find the max and minimum coordinates
+x_max_undistorted = np.max(extreme_points[0])
+x_min_undistorted = np.min(extreme_points[0])
+y_max_undistorted = np.max(extreme_points[1])
+y_min_undistorted = np.min(extreme_points[1])
+
+# Start creating the undistorted image
+x_u = np.zeros((2, x_max_original*y_max_original))
+
+# Create the different x_coordinates and y_coordinates values
+dx = (x_max_undistorted-x_min_undistorted)/x_max_original
+x_coordinates = np.arange(x_min_undistorted, x_max_undistorted, dx) 
+x_coordinates = npm.repmat(x_coordinates, 1, y_max_original).reshape(-1)
+
+dy = (y_max_undistorted-y_min_undistorted)/y_max_original
+y_coordinates = np.arange(y_min_undistorted, y_max_undistorted, dy) 
+y_coordinates = npm.repmat(y_coordinates.reshape(-1,1), 1, x_max_original).reshape(-1)
+
+
+x_u[0] = x_coordinates
+x_u[1] = y_coordinates
+
+#
+# Distort the coordinates 
+#
+# This requires the inverse of the distortion model, i.e. calculate the distances from the distorted undistorted coordinates to the center of distortion(origo)
+# The first step is to calculate the distances from the undistorted coordinates to origo
+r_u = np.linalg.norm(x_u, axis=0)
+
+# The second step is to solve the inverse of the distortion model. This is done according to the paper
+if lambda_ > 0: # pincushion distortion
+    r_d = 1/(2*lambda_*r_u) + (1/(4*lambda_**2*r_u**2) - 1/lambda_)**0.5 # This is the bigger of the solutions. The paper uses the smaller. hmmmm
+else: # barrel distortion
+    r_d = 1/(2*lambda_*r_u) + (1/(4*lambda_**2*r_u**2) - 1/lambda_)**0.5 
+
+# The final step is to calculate the distorted undistorted coordinates
+xd_hat = (r_d/r_u)*x_u
+xd_hat *= (x_max_original+y_max_original)
+xd_hat[0] += x_max_original/2
+xd_hat[1] += y_max_original/2
+xd_hat = np.ndarray.astype(xd_hat, dtype='int')
+
+
+# Remove the coordinates which have no correspondence in the original image
+extract_rule = xd_hat[0] >= 0
+extract_rule = np.logical_and(extract_rule, xd_hat[0] < x_max_original)
+extract_rule = np.logical_and(extract_rule, xd_hat[1] < y_max_original)
+extract_rule = np.logical_and(extract_rule, xd_hat[1] >= 0)
+
+xd_hat = xd_hat[:,np.argwhere(extract_rule).reshape(-1)]
+
+#Extract the pixels which are to be filled in
+pixels_to_fill = np.zeros((2, x_max_original*y_max_original))
+
+x_coordinates = np.arange(x_max_original)
+x_coordinates = npm.repmat(x_coordinates, 1, y_max_original)
+
+y_coordinates = np.arange(y_max_original)
+y_coordinates = npm.repmat(y_coordinates.reshape(-1,1), 1, x_max_original).reshape(-1)
+
+pixels_to_fill[0] = x_coordinates
+pixels_to_fill[1] = y_coordinates
+
+pixels_to_fill = pixels_to_fill[:,np.argwhere(extract_rule).reshape(-1)]
+pixels_to_fill = np.ndarray.astype(pixels_to_fill, dtype='int')
+
+undistorted_image = np.zeros((y_max_original, x_max_original, 3))
+undistorted_image[pixels_to_fill[1], pixels_to_fill[0]] = img1_rgb[xd_hat[1], xd_hat[0]].reshape(-1,3)
+
 cv2.imwrite("undistorted.jpg", undistorted_image) 
 
+print('Done!')
